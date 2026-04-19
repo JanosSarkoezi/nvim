76793dc26fae7d7cb9fe6462351cd6c3c06a8855
Janos Sarkoezi <j.sarkoezi@googlemail.com>
2026-04-19 20:45:07 +0200

    Anpassung der Optionen

--- lua/options.lua
+++ lua/options.lua
@@ -1,33 +1,125 @@
--- lua/options.lua
--- vim.opt Einstellungen
+-- [[ Setting options ]]
+-- See `:help vim.opt`
+-- NOTE: You can change these options as you wish!
+--  For more options, you can see `:help option-list`
 
-local opt = vim.opt
+-- Make line numbers default
+vim.opt.number = true
+-- You can also add relative line numbers, to help with jumping.
+--  Experiment for yourself to see if you like it!
+-- vim.opt.relativenumber = true
 
--- Zeilennummern
-opt.number = true
-opt.relativenumber = true
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.opt.mouse = "a"
+-- vim.opt.shada = ''
 
--- Tab-Einstellungen
-opt.expandtab = true
-opt.shiftwidth = 4
-opt.tabstop = 4
-opt.smartindent = true
+-- Ignore target directories 
+vim.opt.wildignore:append { "**/target/**" }
+-- Suche erst in lokalen tags, dann in den JDK tags
+vim.opt.tags = {
+  "./tags",           -- Tags im aktuellen Verzeichnis (Projekt)
+  "tags",             -- Tags im Arbeitsverzeichnis
+  os.getenv("HOME") .. "/dev/java/src/tags" -- Dein globaler JDK-Index
+}
 
--- Suche
-opt.ignorecase = true
-opt.smartcase = true
-opt.hlsearch = false
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
 
--- UI
-opt.termguicolors = true
-opt.cursorline = true
-opt.scrolloff = 8
-opt.signcolumn = "yes"
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.opt.clipboard = "unnamedplus"
 
--- System
-opt.clipboard = "unnamedplus"
-opt.backup = false
-opt.swapfile = false
-opt.undofile = true
--- opt.updatecall = 300
-opt.timeoutlen = 400
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = false
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default
+vim.opt.signcolumn = "yes"
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 900
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = "split"
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
+vim.opt.expandtab = true
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+
+-- Folding
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldtext = "v:lua.vim.treesitter.foldtext()"
+vim.opt.foldlevel = 99
+
+-- Am ende der Suche nicht neu oben anfangen
+vim.opt.wrapscan = false
+
+-- Grep with external Programms
+-- if vim.fn.executable('rg') > 0 then
+--   vim.o.grepprg =
+--       [[rg --hidden --glob "!.git" --no-heading --smart-case --vimgrep --follow $*]]
+-- elseif vim.fn.executable('ag') > 0 then
+--   vim.o.grepprg = [[ag --nogroup --nocolor --vimgrep]]
+-- end
+
+vim.api.nvim_set_hl(0, "MyColor", {fg="#00FF00"})
+
+local function statusline()
+    local set_color_1 = "%#MyColor#"
+    local file_name = " %f"
+    local set_color_2 = "%#StatusMsg#"
+    local modified = "%m"
+    local align_right = "%="
+    local fileencoding = " %{&fileencoding?&fileencoding:&encoding}"
+    local fileformat = " [%{&fileformat}]"
+    local filetype = " %y"
+    local percentage = " %p%%"
+    local linecol = " %l:%c"
+
+    return string.format(
+        "%s%s %s%s%s%s%s%s%s%s%s",
+        set_color_2,
+        file_name,
+        set_color_1,
+        modified,
+        set_color_2,
+        align_right,
+        filetype,
+        fileencoding,
+        fileformat,
+        percentage,
+        linecol
+    )
+end
+
+vim.opt.statusline = statusline()
+-- vim: ts=2 sts=2 sw=2 et
